778ab5db3c9030b67776cb2bd6c850491c06e626
Jerry Hara <jerrykeiko98@gmail.com>
2023-11-27 12:17:55 -0800

    Finished: sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    let sum = 0;
+    if (min < 0 || max < 0 || !(Number.isInteger(min)) || !(Number.isInteger(max))) return "ERROR";
+    if (min > max){
+        let temp = min;
+        min = max;
+        max = temp;
+    }
 
+    for (let i = min; i <= max; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
