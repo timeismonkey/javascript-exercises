04803982d78ca380c73a882a540afe5c988335a9
Jerry Hara <jerrykeiko98@gmail.com>
2023-11-27 12:34:24 -0800

    Finished: 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // Divisible by 400
+    if (year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0)) return true;
+    return false;
 };
 
 // Do not edit below this line
